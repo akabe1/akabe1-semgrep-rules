08d70685be463f839a15794ea622c5d04080bf6d
Maurizio S <46047144+akabe1@users.noreply.github.com>
2024-02-29 12:23:02 +0100

    Add files via upload

--- ios/swift/injections/nosql_inj_test.swift
+++ ios/swift/injections/nosql_inj_test.swift
@@ -6,13 +6,13 @@ class NoSqlViewController: UIViewController {
 
     func foo1(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         let emailId = message.body as? String
-        // vuln realm NoSQL injection
+        // ruleid: vuln realm NoSQL injection
         let email = realm.objects(Email.self).filter("id == '" + emailId + "'")
     }
 
     
     func foo2() {
-        // good realm NoSQL 
+        // ok: good realm NoSQL 
         let email = realm.objects(Email.self).filter("id == '" + "1234" + "'")
     }
 
