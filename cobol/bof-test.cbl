215e03b0a9f121696fb694128705fd2d8f4f7740
Maurizio S <46047144+akabe1@users.noreply.github.com>
2025-08-09 16:57:04 +0200

    Add files via upload

--- cobol/bof-test.cbl
+++ cobol/bof-test.cbl
@@ -5,13 +5,23 @@
        SOURCE-COMPUTER. IBM-370
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
+           SELECT CUSTOMER-FILE ASSIGN TO CUSTOMER-FILE-NAME
+               ORGANIZATION IS LINE SEQUENTIAL
+               FILE STATUS IS FILE-STATUS.
 
        DATA DIVISION.
+       FILE SECTION.
+       FD CUSTOMER-FILE.
+       01 CUSTOMER-RECORD PIC X(80).
+
 
        WORKING-STORAGE SECTION.
+       77 DATA-PTR             USAGE POINTER.
        77 LONG-INPUT           PIC X(20).
        77 SHORT-FIELD          PIC X(05).
-       77 TINY                 PIC X(20).
+       77 INPUT-USER           PIC X(20).
+       77 INPUT-PASS           PIC X(20).
+       77 TINY                 PIC X(20) BASED.
        77 BIG                  PIC X(50).
        77 SOURCE-FIELD         PIC X(20).
        77 DEST-FIELD           PIC X(10).
@@ -21,26 +31,48 @@
        PROCEDURE DIVISION.
        
 
-      * --- Buffer Overflow ---
+* --- Buffer Overflow ---
+
        DISPLAY "Enter a long string (max 20 chars): ".
        * --- ruleid : vuln bof ---
+       ACCEPT LONG-INPUT.   
+       ALLOCATE 10 CHARACTERS INITIALIZED RETURNING DATA-PTR
+       SET ADDRESS OF SHORT-FIELD TO DATA-PTR                           
+       MOVE LONG-INPUT TO SHORT-FIELD. 
+       
+
+       DISPLAY "Enter a long string (max 20 chars): ".
+       * --- ruleid : ok bof ---
        ACCEPT LONG-INPUT.                              
        MOVE LONG-INPUT TO SHORT-FIELD.                   
 
+
+       DISPLAY "Enter a long string (max 50 chars): ".
+       * --- ruleid : ok bof ---
+       ACCEPT BIG.                              
+       MOVE BIG TO TINY. 
+       
        
        DISPLAY "Enter a long string (max 50 chars): ".
        * --- ruleid : vuln bof ---
-       ACCEPT BIG.                              
-       MOVE BIG TO TINY.   
+       ACCEPT BIG.
+       ALLOCATE TINY INITIALIZED                              
+       MOVE BIG TO TINY.    
        
        
        * --- ruleid : ok bof ---
-       ACCEPT SOURCE-FIELD
+       MOVE "ABCDEFGHIJKLMNOPQRST" TO SOURCE-FIELD
        IF FUNCTION LENGTH(SOURCE-FIELD) <= MAX-LEN
            MOVE SOURCE-FIELD(1:MAX-LEN) TO DEST-FIELD
        ELSE
            DISPLAY "Error: input too big"
        END-IF
-
+       
+       
+       DISPLAY "Enter a long string (max 100 chars): ".
+       * --- ruleid : ok bof ---
+       ACCEPT LONG-INPUT
+       IF FUNCTION LENGTH(LONG-INPUT) <= LENGTH OF SHORT-FIELD
+       MOVE LONG-INPUT TO SHORT-FIELD.
 
        STOP RUN.
